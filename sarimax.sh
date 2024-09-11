be8d8970c4547cb4c4598300a29ca6f28417a9a5
leovallejo <95703028+leovallejo@users.noreply.github.com>
2024-09-12 02:30:49 +0800

    Update sarimax.sh

--- sarimax.sh
+++ sarimax.sh
@@ -7,7 +7,7 @@ BRIGHT_GREEN="\033[1;32m"   # Bright Green for success messages
 MAGENTA="\033[1;35m"        # Magenta for titles
 RESET="\033[0m"             # Reset to default color
 
-echo -e "${LIGHT_BLUE}Upgrade Your Allora Model(Y/N):${RESET}"
+echo -e "${LIGHT_BLUE}Upgrade Your Allora Model to SARIMAX(Y/N):${RESET}"
 read -p "" installdep
 echo
 
@@ -38,4 +38,4 @@ else
 fi
 
 echo
-echo -e "${MAGENTA}==============0xTnpxSGT | Allora===============${RESET}"
+echo -e "${MAGENTA}==============thanks to 0xTnpxSGT | Allora SARIMAX===============${RESET}"
