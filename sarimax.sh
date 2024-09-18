a5da0aa9a09235f10ce6ab7eb795adc38bceb702
leovallejo <95703028+leovallejo@users.noreply.github.com>
2024-09-18 08:47:53 +0800

    Update sarimax.sh

--- sarimax.sh
+++ sarimax.sh
@@ -17,7 +17,7 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
     echo
     rm -rf app.py
     rm -rf requirements.txt
-    wget -q https://raw.githubusercontent.com/leovallejo/Nud/ARIMAMODEL/app.py -O /root/allora-huggingface-walkthrough/app.py
+    wget -q https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/app.py?token=GHSAT0AAAAAACW4AWHZNHQXPQSJTQAIWDZUZXKEM6Q -O /root/allora-huggingface-walkthrough/app.py
     wget -q https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/requirements.txt?token=GHSAT0AAAAAACW4AWHZOGOEUX5IRVZ2HFPGZXKELEQ -O /root/allora-huggingface-walkthrough/requirements.txt
     wait
 	
