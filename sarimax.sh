56bf6669d179fa4006d54d1102a63ac10644e721
leovallejo <95703028+leovallejo@users.noreply.github.com>
2024-09-29 08:35:09 +0800

    Update sarimax.sh

--- sarimax.sh
+++ sarimax.sh
@@ -23,7 +23,11 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
      -o /root/allora-huggingface-walkthrough/app.py && \
     curl -s -H "Authorization: token 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" \
      "https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/requirements.txt" \
-     -o /root/allora-huggingface-walkthrough/requirements.txt 
+     -o /root/allora-huggingface-walkthrough/requirements.txt && \
+     curl -s -H "Authorization: token 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" \
+     "https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/requirements.txt" \
+     -o /root/allora-huggingface-walkthrough/Dockerfile
+     
      
     #FOR CNN MODEL
    # curl -s -H "Authorization: token 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" \
