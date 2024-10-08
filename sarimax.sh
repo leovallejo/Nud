1f4389b4bf5f78c7ca569d93c920f740f9cb3f81
leovallejo <95703028+leovallejo@users.noreply.github.com>
2024-10-08 15:55:39 +0800

    Update sarimax.sh

--- sarimax.sh
+++ sarimax.sh
@@ -7,7 +7,7 @@ BRIGHT_GREEN="\033[1;32m"   # Bright Green for success messages
 MAGENTA="\033[1;35m"        # Magenta for titles
 RESET="\033[0m"             # Reset to default color
 
-echo -e "${LIGHT_BLUE}Upgrade Your Allora Model to SARIMAX(Y/N):${RESET}"
+echo -e "${LIGHT_BLUE}Upgrade Your Allora Model(Y/N):${RESET}"
 read -p "" installdep
 echo
 
@@ -15,17 +15,37 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
 
     echo -e "${LIGHT_BLUE}Clone & Replace old file :${RESET}"
     echo
-   # rm -rf .env
     rm -rf app.py
     rm -rf requirements.txt
-   # rm -rf Dockerfile
-    #rm -rf docker-compose.yaml
-   #NEW MODEL
-   curl -s -H "Authorization: token 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" \
-     https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/app.py \
-     -o /root/allora-huggingface-walkthrough/app.py && \
+    curl -s -H "Authorization: token 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" \
+    https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/app.py \
+    -o /root/allora-huggingface-walkthrough/app.py && \
     curl -s -H "Authorization: token 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" \
-     "https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/requirements.txt" \
+    "https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/requirements.txt" 
+ 
+    wait
+	
+    echo -e "${LIGHT_BLUE}Rebuild and run a model :${RESET}"
+
+    cd /root/allora-huggingface-walkthrough/	
+    echo
+    docker compose up --build -d
+    echo
+	
+    echo
+    docker compose logs -f
+    echo
+	
+else
+    echo -e "${BRIGHT_GREEN}Operation Canceled :${RESET}"
+    
+fi
+
+echo
+echo -e "${MAGENTA}==============Linnea Zeph | Allora===============${RESET}"
+
+
+
     # -o /root/allora-huggingface-walkthrough/requirements.txt && \
      #curl -s -H "Authorization: token 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" \
      #"https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/.env" \
@@ -49,24 +69,3 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
   #   -o /root/allora-huggingface-walkthrough/requirements.txt 
 
      
-    
-    wait
-	
-    echo -e "${LIGHT_BLUE}Rebuild and run a model :${RESET}"
-
-    cd /root/allora-huggingface-walkthrough/	
-    echo
-    docker compose up --build -d
-    echo
-	
-    echo
-    docker compose logs -f 
-    echo
-	
-else
-    echo -e "${BRIGHT_GREEN}Operation Canceled :${RESET}"
-    
-fi
-
-echo
-echo -e "${MAGENTA}==============thanks to 0xTnpxSGT | Allora SARIMAX===============${RESET}"
