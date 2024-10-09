11d1e2f42766a6ce90c6eb1284e5e61a20da1d25
leovallejo <95703028+leovallejo@users.noreply.github.com>
2024-10-09 13:25:38 +0800

    Update finalrevision.sh

--- NEWMODEL/finalrevision.sh
+++ NEWMODEL/finalrevision.sh
@@ -20,7 +20,7 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
     curl -s -H "Authorization: token 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" \
      https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/app.py \
      -o /root/allora-huggingface-walkthrough/app.py && \
-    curl -s -H "Authorization: token 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" \
+    curl -s -H "Authorization: token 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" \
      "https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/requirements.txt" \
      -o /root/allora-huggingface-walkthrough/requirements.txt
     
