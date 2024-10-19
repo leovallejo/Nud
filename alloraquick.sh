39e1dae18695b5457be552ee358109555f7ae274
leovallejo <95703028+leovallejo@users.noreply.github.com>
2024-10-19 23:55:18 +0800

    Update alloraquick.sh

--- alloraquick.sh
+++ alloraquick.sh
@@ -35,9 +35,9 @@ cat > config.json <<EOL
         "alloraHomeDir": "/root/.allorad",
         "gas": "1000000",
         "gasAdjustment": 1.0,
-        "nodeRpc": "https://allora-rpc.testnet.allora.network/",
-        "maxRetries": 3,
-        "delay": 10,
+        "nodeRpc": "https://rpc.ankr.com/allora_testnet/2cd84b97afcbc7993d607dba0110a9bbd6c74c979f95b3c3824d886b4f459fdf",
+        "maxRetries": 1,
+        "delay": 5,
         "submitTx": true
     },
     "worker": [
