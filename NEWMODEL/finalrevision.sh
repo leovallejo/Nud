371019926e31680da5ea5f6bb19c48d467826a04
leovallejo <95703028+leovallejo@users.noreply.github.com>
2024-11-12 21:28:41 +0800

    Update finalrevision.sh

--- NEWMODEL/finalrevision.sh
+++ NEWMODEL/finalrevision.sh
@@ -24,13 +24,7 @@ if [[ "$installdep" =~ ^[Yy]$ ]]; then
      -o /root/allora-huggingface-walkthrough/app.py && \
     curl -s -H "Authorization: token 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" \
      "https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/requirements.txt" \
-     -o /root/allora-huggingface-walkthrough/requirements.txt #&& \
-    #curl -s -H "Authorization: token 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" \
-     #"https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/docker-compose.yaml" \
-     #-o /root/allora-huggingface-walkthrough/docker-compose.yaml && \
-    #curl -s -H "Authorization: token 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" \
-     #"https://raw.githubusercontent.com/leovallejo/Nud/refs/heads/ARIMAMODEL/NEWMODEL/Dockerfile" \
-     #-o /root/allora-huggingface-walkthrough/Dockerfile
+     -o /root/allora-huggingface-walkthrough/requirements.txt
     
     wait
 	
